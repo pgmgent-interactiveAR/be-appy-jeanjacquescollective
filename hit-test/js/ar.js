d0563f0e008d001738cf032461784c588249bc1a
Jannes Lambrecht <janneslambrecht@hotmail.com>
2022-12-23 13:16:17 +0100

    update hit-test

--- hit-test/js/ar.js
+++ hit-test/js/ar.js
@@ -1,15 +1,16 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 const app = () => {
-  init = () => {
-    console.log(navigator.xr)
-    if(navigator.xr){
-    document.querySelector('#startAR').addEventListener('click', activateXR);
-    }
-    else{
-      alert('not comp')
+  const init = () => {
+    console.log(navigator.xr);
+    if (navigator.xr) {
+      document.querySelector('#startAR').addEventListener('click', activateXR);
+    } else {
+      alert('not comp');
     }
   };
 
-  activateXR = async () => {
+  const activateXR = async () => {
     // create canvas and initialize WebGL Context
     const canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
@@ -18,6 +19,19 @@ const app = () => {
     // create scene to draw object on
     const scene = new THREE.Scene();
 
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+    directionalLight.position.set(10, 15, 10);
+    scene.add(directionalLight);
+    const loader = new GLTFLoader();
+    let reticle;
+    loader.load(
+      'https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf',
+      function (gltf) {
+        reticle = gltf.scene;
+        reticle.visible = false;
+        scene.add(reticle);
+      }
+    );
     // materials for cube
     const materials = [
       new THREE.MeshBasicMaterial({ color: 'red' }),
@@ -32,8 +46,8 @@ const app = () => {
       new THREE.BoxGeometry(0.2, 0.2, 0.2),
       materials
     );
-    cube.position.set(0, 0, -1);
-    scene.add(cube);
+    // cube.position.set(0, 0, -1);
+    // scene.add(cube);
 
     const renderer = new THREE.WebGLRenderer({
       alpha: true,
@@ -47,13 +61,27 @@ const app = () => {
     const camera = new THREE.PerspectiveCamera();
     camera.matrixAutoUpdate = false;
 
-      const session = await navigator.xr.requestSession('immersive-ar');
-      
-      session.updateRenderState({
-        baseLayer: new XRWebGLLayer(session, gl),
-      });
+    const session = await navigator.xr.requestSession('immersive-ar', {
+      requiredFeatures: ['hit-test'],
+    });
 
+    session.updateRenderState({
+      baseLayer: new XRWebGLLayer(session, gl),
+    });
+    session.addEventListener('select', (event) => {
+      if (cube) {
+        const clone = cube.clone();
+        clone.position.copy(reticle.position);
+        scene.add(clone);
+      }
+    });
     const referenceSpace = await session.requestReferenceSpace('local');
+
+    const viewerSpace = await session.requestReferenceSpace('viewer');
+
+    const hitTestSource = await session.requestHitTestSource({
+      space: viewerSpace,
+    });
 
     const onXRFrame = (time, frame) => {
       session.requestAnimationFrame(onXRFrame);
@@ -77,13 +105,24 @@ const app = () => {
         camera.projectionMatrix.fromArray(view.projectionMatrix);
         camera.updateMatrixWorld(true);
 
+        const hitTestResults = frame.getHitTestResults(hitTestSource);
+        if (hitTestResults.length > 0 && reticle) {
+          const hitPose = hitTestResults[0].getPose(referenceSpace);
+          reticle.visible = true;
+          reticle.position.set(
+            hitPose.transform.position.x,
+            hitPose.transform.position.y,
+            hitPose.transform.position.z
+          );
+          reticle.updateMatrixWorld(true);
+        }
         renderer.render(scene, camera);
       }
     };
     session.requestAnimationFrame(onXRFrame);
   };
 
-  rotateCube = (cubeToRotate, speedX = 0, speedY = 0) => {
+  const rotateCube = (cubeToRotate, speedX = 0, speedY = 0) => {
     cubeToRotate.rotation.y += speedX;
     cubeToRotate.rotation.x += speedY;
   };
